b0d62b2a4dafc75f4394bce2ddb5ac2e80012e1a
Rick Belson <belsonrj@aol.com>
2020-01-28 04:11:38 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -6,8 +6,8 @@ def create_an_array
   beatles = ["john", "paul", "george", "ringo"]
 end
 
-def add_element_to_end_of_array
-beatles << "billy"
+def add_element_to_end_of_array(beatles)
+  beatles << "billy"
 end
 
 def add_element_to_start_of_array(array, element)
